aaf27f42866c5017e43af37f3fc55f20314d01e9
etudiant <etudiant@Mac-Guillaume.local>
2015-01-05 09:05:44 +0100

    janvier2015-01

--- js/app.js
+++ js/app.js
@@ -12,7 +12,9 @@
 					date: "09 avril ",
 					year: "1901",
 					comment: "09 avril 1901 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.",
-					nameClass: "gare-1901-04-09"
+					namePoster: "XXX",
+					emailPoster: "XXX@xxx.xx",
+					newsletter: false
 			},
 			{
 					titleImg: "La Gare de Mons",
